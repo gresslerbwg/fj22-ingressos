abcdcf44cf145321f4b4e0896352d19dfb219af4
Henrique Gressler <henriquegressler@gmail.com>
2018-11-06 11:30:32 -0200

    Criação das classes Sessao.java e SessaoController.java

---
+++ src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
@@ -0,0 +1,30 @@
+package br.com.caelum.ingresso.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import br.com.caelum.ingresso.dao.FilmeDao;
+import br.com.caelum.ingresso.dao.SalaDao;
+import br.com.caelum.ingresso.model.Sala;
+
+@Controller
+public class SessaoController {
+	
+	@Autowired
+	private SalaDao salaDao;
+	
+	@Autowired
+	private FilmeDao filmeDao;
+
+	@RequestMapping("/admin/sessao")
+	public ModelAndView form(@RequestParam("salaId") Integer salaId){
+		ModelAndView modelAndView = new ModelAndView("sessao/sessao");
+		modelAndView.addObject("sala",salaDao.findOne(salaId));
+		modelAndView.addObject("filmes", filmeDao.findAll());
+		return modelAndView;
+	}
+}
